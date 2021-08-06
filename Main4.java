d751a0a44775a8521c4ef4c7c1d30bbbb9385581
奈良将人 <naramasato@naramasatonoMacBook-Pro.local>
2021-08-06 14:08:07 +0900

    java

--- Main4.java
+++ Main4.java
@@ -1,8 +1,20 @@
 public class Main4 {
   public static void main(String[] args) {
-    int[] scores = {20, 30, 40, 50, 80};
+    int[] scores = { 20, 30, 40, 50, 80 };
+    int sum = 0;
     for (int i = 0; i < scores.length; i++) {
-      System.out.println(scores[i]);
+      sum += scores[i];
     }
+    int avg = sum / scores.length;
+    System.out.println("合計点：" + sum);
+    System.out.println("平均点：" + avg);
+
+    int count = 0;
+    for (int u = 0; u < scores.length; u++) {
+      if (scores[u] >= 50) {
+        count++;
+      }
+    }
+    System.out.println("50点以上の科目の数は" + count);
   }
 }
